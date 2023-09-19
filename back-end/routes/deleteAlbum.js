008e3e21c012a4e11a8494db304fdd788c57c77b
TheDanishMexican <danieljchristjansen@gmail.com>
2023-09-19 11:25:18 +0200

    yes

--- back-end/routes/deleteAlbum.js
+++ back-end/routes/deleteAlbum.js
@@ -6,28 +6,37 @@ const deleteAlbum = express.Router();
 deleteAlbum.delete('/albums/:album_id', (request, response) => {
     const albumIdToDelete = request.params.album_id;
 
-    const deleteTrackAlbumQuery = 'DELETE FROM track_album WHERE album_id = ?';
-
-    database.query(deleteTrackAlbumQuery, [albumIdToDelete], (error, results, fields) => {
+    // Step 1: Delete references in the track_artist table
+    const deleteTrackArtistQuery = 'DELETE FROM track_artist WHERE track_id IN (SELECT track_id FROM tracks WHERE album_id = ?)';
+    database.query(deleteTrackArtistQuery, [albumIdToDelete], (error, results, fields) => {
         if (error) {
             console.log(error);
             response.json({ message: error });
         } else {
-            const deleteTrackQuery = 'DELETE FROM tracks WHERE album_id = ?';
-
-            database.query(deleteTrackQuery, [albumIdToDelete], (error, results, fields) => {
+            // Step 2: Delete references in the track_album table
+            const deleteTrackAlbumQuery = 'DELETE FROM track_album WHERE track_id IN (SELECT track_id FROM tracks WHERE album_id = ?)';
+            database.query(deleteTrackAlbumQuery, [albumIdToDelete], (error, results, fields) => {
                 if (error) {
                     console.log(error);
                     response.json({ message: error });
                 } else {
-                    const deleteAlbumQuery = 'DELETE FROM albums WHERE album_id = ?';
-
-                    database.query(deleteAlbumQuery, [albumIdToDelete], (error, results, fields) => {
+                    // Step 3: Delete tracks associated with the album
+                    const deleteTracksQuery = 'DELETE FROM tracks WHERE album_id = ?';
+                    database.query(deleteTracksQuery, [albumIdToDelete], (error, results, fields) => {
                         if (error) {
                             console.log(error);
                             response.json({ message: error });
                         } else {
-                            response.json({ message: 'Album deleted successfully' });
+                            // Step 4: Delete the album itself
+                            const deleteAlbumQuery = 'DELETE FROM albums WHERE album_id = ?';
+                            database.query(deleteAlbumQuery, [albumIdToDelete], (error, results, fields) => {
+                                if (error) {
+                                    console.log(error);
+                                    response.json({ message: error });
+                                } else {
+                                    response.json({ message: 'Album deleted successfully' });
+                                }
+                            });
                         }
                     });
                 }
@@ -37,3 +46,4 @@ deleteAlbum.delete('/albums/:album_id', (request, response) => {
 });
 
 export default deleteAlbum;
+
